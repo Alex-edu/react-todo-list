3d3d2622804c09224f0122623053ad86f3fd5405
hadabr <alex.dovghii@gmail.com>
2020-06-16 06:29:50 +0200

    Updates

---
+++ precache-manifest.d310b5cc685e42b8be5c058b10737644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710737d32d7fe7721f0640193782f3c9",
+    "url": "/react.js-todo-list/index.html"
+  },
+  {
+    "revision": "1a1516e2f84027fb04f9",
+    "url": "/react.js-todo-list/static/css/main.9e6ccfd3.chunk.css"
+  },
+  {
+    "revision": "42258d0f2aa667cde64f",
+    "url": "/react.js-todo-list/static/js/2.e401f773.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react.js-todo-list/static/js/2.e401f773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1516e2f84027fb04f9",
+    "url": "/react.js-todo-list/static/js/main.f8729b0d.chunk.js"
+  },
+  {
+    "revision": "d1f8a43d1b065227c042",
+    "url": "/react.js-todo-list/static/js/runtime-main.0ade2747.js"
+  }
+]);
